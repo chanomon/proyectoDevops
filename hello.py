aa8bfb544b8856b296af1edacb9f79840445b456
Elizandro Huipe <hdomeli@gmail.com>
2025-09-07 13:49:05 -0600

    modificación del script para ver cambios

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@
 
 def main():
     print("Test exitoso")
-    print("Hola Mundo desde GitHub Actions!")
+    print("Hola Mundo desde GitHub Actions!Aquí va mi comment")
 
 if __name__ == "__main__":
     main()
